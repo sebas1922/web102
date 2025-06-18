96cf8548f21e24b9de2fd02426de62975fd570e0
Sebas <sebas@MacBookPro.attlocal.net>
2025-06-17 23:15:47 -0400

    first commit

---
+++ course/week2/samosa/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <div className="Header">
+        <h1>Samosa Selector</h1>
+        <h2>Count:</h2>
+        <img className="samosa" src="./assets/image.png"></img>
+      </div>
+    </div>
+  )
+}
+
+export default App
